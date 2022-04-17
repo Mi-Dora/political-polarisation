b237171aa788cd20247670b3e85a3f63adf4b504
ziffer <3170105860@zju.edu.cn>
2022-04-17 14:09:46 +0800

    update

---
+++ BERT/main.py
@@ -0,0 +1,38 @@
+import os
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from transformers import BertForSequenceClassification, AutoModel, AutoTokenizer, BertTokenizer, BertForMaskedLM, AdamW, Trainer, TrainingArguments
+from datasets import Dataset
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+import logging
+import argparse
+import torch
+import torch.nn as nn
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Add models\' arguments')
+    parser.add_argument('--type', default='bt',
+                    help='use different pretrained model to train and test')
+    parser.add_argument('--qid', default='1')
+    parser.add_argument('--ep', default='10', help='number of epochs')
+    args = parser.parse_args()
+
+    if args.type == 'bt':
+        model_name = 'bert-base-uncased'
+    elif args.type == 'rb':
+        model_name = 'roberta-base'
+    elif args.type == 'simbt':
+        model_name = "princeton-nlp/sup-simcse-bert-base-uncased"
+    elif args.type == 'simrb':
+        model_name = "princeton-nlp/sup-simcse-roberta-base"
+    else :
+        model_name = args.type
---
+++ BERT/towards_biden.py
@@ -0,0 +1,29 @@
+import re
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+import torch
+import numpy as np
+
+# choose GPU if available
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# select mode path here
+pretrained_LM_path = "kornosk/bert-election2020-twitter-stance-biden-KE-MLM"
+
+# load model
+tokenizer = AutoTokenizer.from_pretrained(pretrained_LM_path)
+model = AutoModelForSequenceClassification.from_pretrained(pretrained_LM_path)
+
+id2label = {
+    0: "AGAINST",
+    1: "FAVOR",
+    2: "NONE"
+}
+
+sentence = 'This pandemic has shown us clearly the vulgarity of our healthcare system. Highest costs in the world, yet not enough nurses or doctors. Many millions uninsured, while insurance company profits soar. The struggle continues. Healthcare is a human right. Medicare for all.'
+inputs = tokenizer(sentence.lower(), return_tensors="pt")
+outputs = model(**inputs)
+predicted_probability = torch.softmax(outputs[0], dim=1)[0].tolist()
+
+classifier = pipeline('sentiment-analysis')
+result = classifier(sentence)
+print('result', result)
